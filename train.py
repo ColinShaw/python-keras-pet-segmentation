c4dbb1246699026bf300a10bbebe0f21d9de09c1
Colin Shaw <colin.shaw@aya.yale.edu>
2017-12-29 07:27:33 -0600

    Changed skip layer definition, added support image

--- train.py
+++ train.py
@@ -7,31 +7,33 @@ from   keras.preprocessing       import image
 from   keras.preprocessing.image import ImageDataGenerator
 
 
-
 def generate_model():
     vgg16 = VGG16(weights='imagenet', include_top=False)
+    for layer in vgg16.layers:
+        layer.trainable = False
 
-    c5  = Conv2D(1,(1,1))(vgg16.layers[18].output)
-    u5  = Conv2DTranspose(1,4,strides=(2,2))(c5)
-    c4  = Conv2D(1,(1,1))(vgg16.layers[14].output)
-    s4  = Add()([u5, c4])
-    u4  = Conv2DTranspose(1,4,strides=(2,2))(s4)
-    c1  = Conv2D(1,(1,1))(vgg16.layers[3].output)
-    s1  = Add()([u4, c1])
-    out = Conv2DTranspose(1,16,strides=(8,8))(s1)
+    l5 = vgg16.get_layer('block5_pool').output
+    c5 = Conv2D(1,(1,1))(l5)
+    t5 = Conv2DTranspose(1,4,strides=(2,2))(c5)
+    l4 = vgg16.get_layer('block4_pool').output
+    c4 = Conv2D(1,(1,1))(l4)
+    a4 = Add()([t5, c4])
+    t4 = Conv2DTranspose(1,4,strides=(2,2))(a4)
+    l1 = vgg16.get_layer('block1_pool').output
+    c1 = Conv2D(1,(1,1))(l1)
+    a1 = Add()([t4, c1])
+    t1 = Conv2DTranspose(1,16,strides=(8,8))(a1)
 
-    m = Model(inputs=vgg16.input, outputs=out)
+    model = Model(inputs=vgg16.input, outputs=t1)
 
-    for i in range(19):
-        m.layers[i].trainable = False
-
-    m.compile(
+    model.compile(
         optimizer = Adam(lr=1e-5), 
         loss      = 'categorical_crossentropy', 
         metrics   = ['accuracy']
     )
+    model.summary()
 
-    return m
+    return model
 
 
 def load_image(path):
@@ -42,9 +44,17 @@ def load_data():
     pass
 
 
-
+gen = ImageDataGenerator(
+    rescale            = 1.0 / 255,
+    rotation_range     = 90,
+    width_shift_range  = 0.2,
+    height_shift_range = 0.2,
+    shear_range        = 0.2,
+    zoom_range         = 0.2,
+    horizontal_flip    = True,
+    fill_mode          = 'nearest'
+)
 
 
 model = generate_model()
-model.summary()
 
