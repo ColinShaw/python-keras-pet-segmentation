9a26841f4c60de74fefeedccf038416e69cd92c2
Colin Shaw <colin.shaw@aya.yale.edu>
2017-12-31 14:07:34 -0600

    Fixed some tensor size issues

--- src/model.py
+++ src/model.py
@@ -15,17 +15,17 @@ class Model(object):
 
         v5 = vgg16.get_layer('block5_pool')
         c5 = Conv2D(1,(1,1))(v5.output)
-        t5 = Conv2DTranspose(1,4,strides=(2,2))(c5)
+        t5 = Conv2DTranspose(1,4,strides=(2,2),padding='same')(c5)
 
         v4 = vgg16.get_layer('block4_pool')
         c4 = Conv2D(1,(1,1))(v4.output)
         a4 = Add()([t5, c4])
-        t4 = Conv2DTranspose(1,4,strides=(2,2))(a4)
+        t4 = Conv2DTranspose(1,4,strides=(2,2),padding='same')(a4)
 
         v1 = vgg16.get_layer('block1_pool')
         c1 = Conv2D(1,(1,1))(v1.output)
         a1 = Add()([t4, c1])
-        t1 = Conv2DTranspose(1,16,strides=(8,8))(a1)
+        t1 = Conv2DTranspose(1,16,strides=(8,8),padding='same')(a1)
 
         m = KerasModel(inputs=vgg16.input, outputs=t1)
         i = IOU(1.0)
